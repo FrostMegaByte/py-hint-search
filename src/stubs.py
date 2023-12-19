c21d877175ad52facad58939ca29b8022442a0cc
Mark Bekooy <mark.bekooy@gmail.com>
2023-12-19 15:18:36 +0100

    Use libcst matchers in stubs.py instead of isinstanceof

--- src/stubs.py
+++ src/stubs.py
@@ -36,34 +36,36 @@ class StubTransformer(cst.CSTTransformer):
         else:
             return cst.parse_statement("...")
 
-    def leave_Module(
-        self, original_node: cst.Module, updated_node: cst.Module
-    ) -> cst.Module:
-        """Remove everything from the body that is not an import, class def or function def."""
-        newbody = [
-            node
-            for node in updated_node.body
-            if any(
-                isinstance(node, cls)
-                for cls in [cst.ClassDef, cst.FunctionDef, cst.SimpleStatementLine]
-            )
-        ]
-        return updated_node.with_changes(body=newbody)
-
     def leave_SimpleStatementLine(
         self,
         original_node: cst.SimpleStatementLine,
         updated_node: cst.SimpleStatementLine,
     ) -> cst.CSTNode:
-        newbody = [
-            node
-            for node in updated_node.body
-            if any(isinstance(node, cls) for cls in [cst.Import, cst.ImportFrom])
-            or (
-                isinstance(node, cst.AnnAssign)
-                and m.matches(
-                    node.annotation, m.Annotation(annotation=m.Name("TypeAlias"))
+        """Only keep import and type alias statements."""
+        keep_statement = False
+        if m.matches(updated_node.body[0], m.Import() | m.ImportFrom()):
+            keep_statement = True
+        elif m.matches(
+            updated_node.body[0],
+            m.AnnAssign(annotation=m.Annotation(annotation=m.Name("TypeAlias"))),
+        ):
+            keep_statement = True
+
+        if keep_statement:
+            return updated_node
+        return updated_node.with_changes(body=())
+
+    def leave_Module(
+        self, original_node: cst.Module, updated_node: cst.Module
+    ) -> cst.Module:
+        """Remove everything that is not an import, type alias, class def or function def."""
+        newbody = tuple(
+            [
+                node
+                for node in updated_node.body
+                if m.matches(
+                    node, m.ClassDef() | m.FunctionDef() | m.SimpleStatementLine()
                 )
-            )
-        ]
-        return updated_node.with_changes(body=newbody)
+            ]
+        )
+        return updated_node.with_changes(body=tuple(newbody))
