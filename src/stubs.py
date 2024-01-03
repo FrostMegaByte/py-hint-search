af6fc021f3448d387411b27b0f0f40bcc1e676aa
Mark Bekooy <mark.bekooy@gmail.com>
2024-01-03 12:35:02 +0100

    Remove stubgen, because it doesn't work on deeply nested directories

--- src/stubs.py
+++ src/stubs.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import libcst as cst
 import libcst.matchers as m
 from libcst import RemoveFromParent
@@ -97,11 +96,11 @@ class StubTransformer(cst.CSTTransformer):
 
 
 def create_stub_file(
-    source_code_tree,
-    typed_path,
-    relative_path,
-    file_name,
-    remove_source_code_file=False,
+    source_code_tree: cst.Module,
+    typed_path: str,
+    relative_path: str,
+    file_name: str,
+    remove_source_code_file: bool = False,
 ) -> None:
     # Write the type annotated source code to a file
     output_typed_source_directory = os.path.abspath(
@@ -111,31 +110,15 @@ def create_stub_file(
     source_code_file = os.path.join(output_typed_source_directory, file_name)
     open(source_code_file, "w", encoding="utf-8").write(source_code_tree.code)
 
-    # Use stubgen to create a stub file
+    transformer = StubTransformer()
+    type_annotated_stub_tree = source_code_tree.visit(transformer)
+
+    # Write the type annotated stub to a file
     output_typed_directory = os.path.abspath(os.path.join(typed_path, relative_path))
-    stubgen_result = subprocess.run(
-        [
-            "poetry",
-            "run",
-            "stubgen",
-            "--output",
-            output_typed_directory,
-            source_code_file,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    if stubgen_result.stderr:
-        print("Stubgen failed, so creating stub using own stub generator.")
-        transformer = StubTransformer()
-        type_annotated_stub_tree = source_code_tree.visit(transformer)
-
-        # Write the type annotated stub to a file
-        os.makedirs(output_typed_directory, exist_ok=True)
-        open(
-            os.path.join(output_typed_directory, file_name + "i"), "w", encoding="utf-8"
-        ).write(type_annotated_stub_tree.code)
+    os.makedirs(output_typed_directory, exist_ok=True)
+    open(
+        os.path.join(output_typed_directory, file_name + "i"), "w", encoding="utf-8"
+    ).write(type_annotated_stub_tree.code)
 
     if remove_source_code_file:
         os.remove(source_code_file)
